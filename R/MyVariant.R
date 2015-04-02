002aec7fa4da3fac4f6579448545435463be9da2
amarkscripps <Amark@wireless-123-14.scripps.edu>
2015-04-02 10:53:59 -0700

    url change

--- R/MyVariant.R
+++ R/MyVariant.R
@@ -6,7 +6,7 @@ version <- '0.5'
 
 MyVariant <- setClass("MyVariant",
     slots=list(base.url="character", delay="numeric", step="numeric", version="character", verbose="logical", debug="logical"),
-    prototype=list(base.url="http://myvariant.info/v1", delay=1, step=1000, version=version, verbose=TRUE, debug=FALSE))
+    prototype=list(base.url="http://su08:8000/v1", delay=1, step=1000, version=version, verbose=TRUE, debug=FALSE))
 
 validMyVariantObject <- function(object) {
     errors <- character(0)
